78c1766316b67067f491b923a8d622e45d2b8e8c
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-28 14:48:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+    
+    <!-- Bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container mt-3">
+        <h1>Strong Password Generator</h1>
+        <!-- Form -->
+        <form method="GET">
+            <label for="password-length" class="form-label">Inserisci lunghezza password</label>
+            <input type="number" class="form-control mb-3" name="password-length" id="password-length">
+            <button class="btn btn-primary">Genera password</button>
+        </form>
+    </div>
+</body>
+</html>
